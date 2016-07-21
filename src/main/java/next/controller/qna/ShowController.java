d76451ca4fb1f94c9ac9ee7a990449d5fd9c5ddb
jojoldu <jojoldu@gmail.com>
2016-07-21 21:01:29 +0900

    5번문제 해결

--- src/main/java/next/controller/qna/ShowController.java
+++ src/main/java/next/controller/qna/ShowController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import next.dao.AnswerDao;
 import next.dao.QuestionDao;
@@ -11,6 +12,7 @@ import next.model.Answer;
 import next.model.Question;
 import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
+import next.model.User;
 
 public class ShowController extends AbstractController {
 	private QuestionDao questionDao = new QuestionDao();
@@ -24,10 +26,14 @@ public class ShowController extends AbstractController {
 		
 		question = questionDao.findById(questionId);
 		answers = answerDao.findAllByQuestionId(questionId);
-		
+		HttpSession session = req.getSession();
 		ModelAndView mav = jspView("/qna/show.jsp");
 		mav.addObject("question", question);
 		mav.addObject("answers", answers);
+		if(session.getAttribute("user") != null){
+			mav.addObject("loginUser", (User) session.getAttribute("user"));
+		}
+
 		return mav;
 	}
 }
