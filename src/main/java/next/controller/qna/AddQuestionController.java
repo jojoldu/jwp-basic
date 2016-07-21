67000a14df9e0845b69bcb44c43a235223097d04
jojoldu <jojoldu@gmail.com>
2016-07-21 20:50:42 +0900

    4번문제 해결

--- src/main/java/next/controller/qna/AddQuestionController.java
+++ src/main/java/next/controller/qna/AddQuestionController.java
@@ -6,12 +6,16 @@ import next.dao.AnswerDao;
 import next.dao.QuestionDao;
 import next.model.Answer;
 import next.model.Question;
+import next.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Date;
+
+import static next.controller.UserSessionUtils.isLogined;
 
 /**
  * Created by jojoldu@gmail.com on 2016-07-21.
@@ -23,9 +27,13 @@ public class AddQuestionController extends AbstractController {
 
     @Override
     public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
-        //questionId, writer, title, contents, createdDate, countOfAnswer
+        HttpSession session = req.getSession();
+        if(!isLogined(session)){
+            return jspView("redirect:/");
+        }
+        User user = (User)session.getAttribute("user");
         Question question = new Question(questionDao.findLastIndex()+1,
-                req.getParameter("writer"),
+                user.getName(),
                 req.getParameter("title"),
                 req.getParameter("contents"),
                 new Date(),
