fe8f006fa2f2d642505215394ba34fcaf360ff46
jojoldu <jojoldu@gmail.com>
2016-07-28 22:31:05 +0900

    [#10일차] MVC 프레임워크 제작중 (iniliaze() 작업중)

--- src/main/java/core/nmvc/AnnotationHandlerMapping.java
+++ src/main/java/core/nmvc/AnnotationHandlerMapping.java
@@ -1,14 +1,23 @@
 package core.nmvc;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
 import com.google.common.collect.Maps;
 
+import core.annotation.Controller;
 import core.annotation.RequestMethod;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
+	private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 	private Object[] basePackage;
 	
 	private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -18,7 +27,21 @@ public class AnnotationHandlerMapping {
 	}
 	
 	public void initialize() {
-		
+		for(Object packgeName : this.basePackage){
+			Reflections reflections = new Reflections((String)packgeName);
+			Set<Constructor> constructors = reflections.getConstructorsAnnotatedWith(Controller.class);
+			for(Constructor constructor : constructors){
+				try {
+					Object obj = constructor.newInstance();
+					Method[] methods = obj.getClass().getDeclaredMethods();
+					for(Method method : methods){
+						//@RequestMapping 선언된것인지 확인후 handlerExecutions에 등록
+					}
+				} catch (Exception e) {
+					logger.error("AnnotationHandlerMapping.initialize()", e);
+				}
+			}
+		}
 	}
 	
 	public HandlerExecution getHandler(HttpServletRequest request) {
