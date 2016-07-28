d9c4e3b256d17af48620a359805f722da2b610ac
jojoldu <jojoldu@gmail.com>
2016-07-28 22:00:51 +0900

    [#10일차] User의 인스턴스를 Reflection 을 사용해서 생성해보기

--- src/test/java/next/model/UserTest.java
+++ src/test/java/next/model/UserTest.java
@@ -1,7 +1,25 @@
 package next.model;
 
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class UserTest {
 	public static User newUser(String userId) {
 		return new User(userId, "password", "name", "test@sample.com");
+	}
+
+	@Test
+	public void run() throws Exception {
+		Class<User> clazz = User.class;
+		Constructor[] constructors = clazz.getDeclaredConstructors();
+		Object obj = constructors[0].newInstance("admin", "password", "name", "test@sample.com");
+		if(obj instanceof User){
+			User user = (User) obj;
+			assertThat(user.getName(), is("name"));
+		}
 	}
 }
