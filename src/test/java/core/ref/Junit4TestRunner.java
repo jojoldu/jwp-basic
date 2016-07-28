e17c72e868759537333ac21d81c74faee4173bbe
jojoldu <jojoldu@gmail.com>
2016-07-28 21:39:41 +0900

    [#10일차] Junit4에서 @MyTest가 있는 메소드만 실행시키기

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -2,10 +2,18 @@ package core.ref;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
 	@Test
 	public void run() throws Exception {
 		Class<Junit4Test> clazz = Junit4Test.class;
+		Method[] methods = clazz.getDeclaredMethods();
 
+		for(Method method : methods){
+			if(method.isAnnotationPresent(MyTest.class)){
+				method.invoke(clazz.newInstance());
+			}
+		}
 	}
 }
